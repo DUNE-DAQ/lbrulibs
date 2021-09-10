d30bbadfc015d5387bf8c94c597d88ef811abd94
Krzysztof Furman <k.furman@qmul.ac.uk>
2021-09-10 10:36:18 +0100

    Testing basics of a frame, not sure how to test get() functions without having a received message with current setup

--- unittest/PacmanFrame_test.cxx
+++ unittest/PacmanFrame_test.cxx
@@ -12,26 +12,25 @@
 
 #include "boost/test/unit_test.hpp"
 
-using namespace dunedaq::dataformats;
 
 BOOST_AUTO_TEST_SUITE(PacmanFrame_test)
 
 BOOST_AUTO_TEST_CASE(PacmanHeader_Methods)
 {
-  PACMANFrame frame;
-  PACMANFrame::PACMANMessageHeader header;
-
-  header.type = 0x44;
+  dunedaq::dataformats::PACMANFrame::PACMANMessageHeader header;
+  
   header.unix_ts = 0x0;
   header.words = 0x1;
 
-  BOOST_REQUIRE_EQUAL(header.type, 0x44);
   BOOST_REQUIRE_EQUAL(header.unix_ts, 0x0);
   BOOST_REQUIRE_EQUAL(header.words, 0x1);
 
   // Here we could test all the "get" functions, but for us they take a message as an argument... Perhaps write/take an example message to cast?
-  //void message = D\x89\xde\x1ca\x00\x01\x00D\x01\xd0\x1f%\x02\x00\x00/\x00@\x00\x00\x00\x00@;
+  //void message = "D\x89\xde\x1ca\x00\x01\x00D\x01\xd0\x1f%\x02\x00\x00/\x00@\x00\x00\x00\x00@"; //this doesn't work
   // Decoded as (('DATA', 1629281929, 1), [('DATA', 1, 35987408, b'/\x00@\x00\x00\x00\x00@')])
+  //auto castMessage = reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&message);
+  //BOOST_REQUIRE_EQUAL(castMessage->get_msg_header((void*)&message)->unix_ts,1629281929);
+
   //BOOST_REQUIRE_EQUAL(frame->get_msg_header(message)->unix_ts),0x89)
 }
 
