8b9a42217bd8e1a184bac1dbbf8a49b1bbe9af67
Krzysztof Furman <k.furman@qmul.ac.uk>
2021-09-13 14:34:57 +0100

    Prepared test message, comparing result doesn't work, some dereferencing issue.

--- unittest/PacmanFrame_test.cxx
+++ unittest/PacmanFrame_test.cxx
@@ -7,6 +7,7 @@
  */
 
 #include "dataformats/pacman/PACMANFrame.hpp"
+#include <string>
 
 #define BOOST_TEST_MODULE PacmanFrame_test // NOLINT
 
@@ -25,13 +26,23 @@ BOOST_AUTO_TEST_CASE(PacmanHeader_Methods)
   BOOST_REQUIRE_EQUAL(header.unix_ts, 0x0);
   BOOST_REQUIRE_EQUAL(header.words, 0x1);
 
-  // Here we could test all the "get" functions, but for us they take a message as an argument... Perhaps write/take an example message to cast?
-  //void message = "D\x89\xde\x1ca\x00\x01\x00D\x01\xd0\x1f%\x02\x00\x00/\x00@\x00\x00\x00\x00@"; //this doesn't work
-  // Decoded as (('DATA', 1629281929, 1), [('DATA', 1, 35987408, b'/\x00@\x00\x00\x00\x00@')])
-  //auto castMessage = reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&message);
-  //BOOST_REQUIRE_EQUAL(castMessage->get_msg_header((void*)&message)->unix_ts,1629281929);
+  //Here we could test all the "get" functions, but for us they take a message as an argument... Perhaps write/take an example message to cast?
+  //Message received: b'D\xb0D?a\x00\x01\x00D\x01\xd0\x1f%\x02\x00\x00/\x00@\x00\x00\x00\x00@' //this is wrong - some get interpreted by python print()
+  //0x44 0xb0 0x44 0x3f 0x61 0x0 0x1 0x0 0x44 0x1 0xd0 0x1f 0x25 0x2 0x0 0x0 0x2f 0x0 0x40 0x0 0x0 0x0 0x0 0x40 //this is wrong - wrong order of bytes!
+  //to get 1631536304 the order should be 0x61 0x3f 0x44 0xb0 in the timestamp
+  //trying to correct the order
+  //type ... timestamp ... empty word count empty ... word type... word index ... word timestamp ... data (whatever)
+  //0x44 ... 0x61 0x3f 0x44 0xb0 ... 0x0 0x1 0x0 ... 0x44 ... 0x1 ... 0x2 0x25 0x1f 0xd0 ... 0x0 0x0 0x2f 0x40 0x40 0x0 0x0 0x0 0x0 0x40
+  //Decoded as (('DATA', 1631536304, 1), [('DATA', 1, 35987408, b'/\x00@\x00\x00\x00\x00@')])
 
-  //BOOST_REQUIRE_EQUAL(frame->get_msg_header(message)->unix_ts),0x89)
+  //std::string message = "0x44\xb0\x44\x3f\x61\x0\x1\x0\x44\x1\xd0\x1f\x25\x2\x0\x0\x2f\x0\x40\x0\x0\x0\x0\x40";
+  std::string message = "0x44\x61\x3f\x44\xb0\x0\x1\x0\x44\x1\x2\x25\x1f\xd0\x0\x0\x2f\x40\x40\x0\x0\x0\x0\x40";
+  auto frame = reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&message);
+  auto timestamp = frame->get_msg_header((void*)&message)->unix_ts;
+  auto fHeader = *frame->get_msg_header(&message);
+  BOOST_REQUIRE_EQUAL(frame->get_msg_header((void*)&message)->unix_ts,1631536304);
+  BOOST_REQUIRE_EQUAL(timestamp,1631536304);
+  BOOST_REQUIRE_EQUAL(fHeader.unix_ts,1631536304);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
