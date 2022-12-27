efeaffd07172feab8fc22bf62dec780d9e863d9a
jtenavidal <jtenavid@fnal.gov>
2022-12-27 07:46:36 -0600

    calling new load message function. This new function is used to accomodate variable size frames

--- src/STREAMLinkModel.hpp
+++ src/STREAMLinkModel.hpp
@@ -224,10 +224,7 @@ private:
               try {
 		TargetPayloadType* Payload = new TargetPayloadType();
 		m_timestamp = Payload->get_timestamp() ; 
-
 		Payload -> load_message(msg.data(), msg.size()) ; 
-
-		std::memcpy(static_cast<void *>(&Payload->data), msg.data(), msg.size());
                 m_sink_queue->send(std::move(*Payload), m_sink_timeout);
 		m_packetsizesum += msg.size(); //sum of data from packets
 	       	m_packetsize = msg.size(); //last packet size
