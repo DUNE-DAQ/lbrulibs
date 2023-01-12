0e56afd44c8326b01957218ba66313607a69d880
Naseem Khan <nkhan@dunegpvm02.fnal.gov>
2023-01-12 06:48:53 -0600

    Updated data emulator testing scripts

--- test/toad/encoder_test1.py
+++ test/toad/encoder_test1.py
@@ -2,7 +2,37 @@ from encoder import encode_hdr, encode_data, encoder
 import time
 from random import gauss, uniform
 import numpy as np
+import sys
 
+for i in range(int(sys.argv[1])):
+    timestamp = int(time.time())
+    hdr_par = 1
+    data_par = 0
+    res = 0
+    idd = 1
+
+    clustercount = 0
+
+    file = "testdata_1hdr_" + str(i) + ".txt"
+    file2 = "testdata_bytes_1hdr_"+str(i)+".txt"
+    file3 = "testdata_32bit_1hdr_"+str(i)+".txt"
+
+    f = open(file, "w")
+    x = open(file2, "w")
+    h = open(file3, "w")
+
+    fec_num = int(uniform(0, 511))
+    channel_id = int(uniform(0, 31))
+    num_clusters = np.random.poisson(5)
+    print(clustercount)
+    padd = 6 - (num_clusters % 6)
+    encoder(file, file2, file3, fec_num, num_clusters, channel_id, timestamp, hdr_par, data_par, padd, res, idd)
+    f.close()
+    x.close()
+    h.close()
+    time.sleep(0.5)
+
+'''
 timestamp = int(time.time())
 hdr_par = 1
 data_par = 0
@@ -24,7 +54,8 @@ count = 0
 for i in range(525):
     fec_num = int(uniform(0, 511)) 
     channel_id = int(uniform(0, 31))
-    '''
+'''
+'''
     if(count>4072 and count <4096):
         z = 4096 - count - 8
         print(z)
@@ -33,7 +64,8 @@ for i in range(525):
         padd = 6 - (num_clusters % 6)
         encoder(file, file2,file3,fec_num, num_clusters, channel_id, timestamp, hdr_par, data_par, padd, res, idd)
         break
-    '''
+'''
+'''
     num_clusters = np.random.poisson(5)
     clustercount += num_clusters
     print(clustercount)
@@ -41,6 +73,7 @@ for i in range(525):
     padd = 6 - (num_clusters % 6)
     encoder(file, file2, file3, fec_num, num_clusters, channel_id, timestamp, hdr_par, data_par, padd, res, idd)
     count = count + 8 + 8*int((num_clusters+5)/6)
+'''
 '''
 for i in range(0, 64, 1):
     y = gauss(5, 5)
@@ -60,6 +93,8 @@ for i in range(0, 64, 1):
         encoder(file, file2,file3,fec_num, num_clusters, channel_id, timestamp, hdr_par, data_par, padd, res, idd)
         break
 '''
+'''
 f.close()
 x.close()
 h.close()
+'''
