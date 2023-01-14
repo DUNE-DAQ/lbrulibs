8335d675b9b0ae373ae01f979765cd46477f207d
Kurt_Biery <biery@fnal.gov>
2023-01-13 19:13:45 -0600

    Reduced the sleep in client_send_looped.py from 1.0 to 0.75 seconds, just to be sure that new frames are sent before the readout code DataRequest times out

--- test/toad/client_send_looped.py
+++ test/toad/client_send_looped.py
@@ -5,7 +5,7 @@ import sys
 for i in range(int(sys.argv[1])): #argument in command line is number of txt files to send
     arg = "testdata_bytes_1hdr_"+str(i)+".txt"
     subprocess.call(["./encoded_client_test.out", arg])
-    time.sleep(1)
+    time.sleep(0.75)
     print("one loop finished")
 
 
